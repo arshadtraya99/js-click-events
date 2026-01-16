7bd01cc80078325973ba931b39c8fdc69631add9
arshadtraya99 <arshadtraya18@gmail.com>
2026-01-16 21:28:14 +0530

    Added my file

---
+++ script.js
@@ -0,0 +1,25 @@
+// function changeText() {
+//     let p = document.querySelector("#para");
+//     p.textContent = "Hello Arshad";
+// }
+
+// let p = document.getElementById("para");
+// p.addEventListener("click" , changeText);
+
+
+//=======Preventing Default Behaviour=======
+// let a = document.querySelector("a");
+
+// function prevention(event) {
+//     event.preventDefault();
+// }
+// a.addEventListener("click" , prevention);
+
+let para = document.querySelectorAll("p");
+function alerting(i) {
+    alert(`You have clicked on Para : ${i+1}`);
+}
+para.forEach((el , i) => {
+    el.addEventListener("click" , () => alerting(i));
+    console.log(el)
+})
